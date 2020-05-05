1cc05f79a809c8c1e314a076bb9d1b8b3d8ee7d4
sierra <haleyalton93@gmail.com>
2020-05-05 07:24:00 -0700

    trying add edits to buttons

--- java.js
+++ java.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
     $("#time-display").text("current time with moment")
+});
+$(document).ready(function () {
+    $('.editbtn').click(function () {
+        $(this).html($(this).html() == 'edit' ? 'modify' : 'edit');
+    });
+});
+
+
     //other functions function that saves the event to local storage
     // calculate colors for color of schedule block based on current time 
+    //function to insert saved events
     //
 
 })
