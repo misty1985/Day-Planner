b2fcd39105ec8e6aa45f092b928f1856663dbe0a
sierra <haleyalton93@gmail.com>
2020-05-04 17:18:09 -0700

    adding java and editing html

---
+++ java.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#time-display").text("current time with moment")
+    //other functions function that saves the event to local storage
+    // calculate colors for color of schedule block based on current time 
+    //
+
+})
